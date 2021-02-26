c807c0db5bc1b2b44bf2a2b81098a394decefec2
Víctor <vivivikthor@gmail.com>
2021-02-26 15:21:20 +0100

    Dovi's cut

--- js/accordion.js
+++ js/accordion.js
@@ -4,6 +4,10 @@ let accordionButtons = document.querySelectorAll('[id^=accordion-button-]');
 let accordionSections = document.querySelectorAll('[id^=accordion-section-]');
 let accordionButtonDeco = document.querySelectorAll('.btn.read .deco span:nth-of-type(2)');
 
+
+let MobNavLink = document.querySelectorAll("#app-bar__menu li a");
+
+
 accordionButtons.forEach(function (item, index) {
     item.setAttribute('aria-controls', accordionSections[index].id);
     item.setAttribute('aria-expanded', 'false');
@@ -51,6 +55,49 @@ mainNavLinks[5].onclick = function(){
 
   // GOALS ACTIVATION FROM NAV //
 mainNavLinks[6].onclick = function(){
+    accordionSections[5].classList.add('active');
+    accordionButtonDeco[5].classList.remove('vertical');
+    if (accordionButtons[5].getAttribute('aria-expanded') === 'false') {
+        accordionButtons[5].setAttribute('aria-expanded', 'true');
+        accordionSections[5].focus();
+    } else {
+        accordionButtons[5].setAttribute('aria-expanded', 'false');
+        accordionSections[5].blur();
+    }
+  }
+
+  // ACTIVATIONS FROM MOBILE //
+
+  
+
+  // BUDGET ACTIVATION FROM NAV //
+  MobNavLink[4].onclick = function(){
+    accordionSections[3].classList.add('active');
+    accordionButtonDeco[3].classList.remove('vertical');
+    if (accordionButtons[3].getAttribute('aria-expanded') === 'false') {
+        accordionButtons[3].setAttribute('aria-expanded', 'true');
+        accordionSections[3].focus();
+    } else {
+        accordionButtons[3].setAttribute('aria-expanded', 'false');
+        accordionSections[3].blur();
+    }
+  }
+
+  // COMMITMENT ACTIVATION FROM NAV //
+MobNavLink[5].onclick = function(){
+    accordionSections[4].classList.add('active');
+    accordionButtonDeco[4].classList.remove('vertical');
+    if (accordionButtons[4].getAttribute('aria-expanded') === 'false') {
+        accordionButtons[4].setAttribute('aria-expanded', 'true');
+        accordionSections[4].focus();
+    } else {
+        accordionButtons[4].setAttribute('aria-expanded', 'false');
+        accordionSections[4].blur();
+    }
+  }
+
+  // GOALS ACTIVATION FROM NAV //
+MobNavLink[6].onclick = function(){
     accordionSections[5].classList.add('active');
     accordionButtonDeco[5].classList.remove('vertical');
     if (accordionButtons[5].getAttribute('aria-expanded') === 'false') {
--- js/nav-scroll.js
+++ js/nav-scroll.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var mainNavLinks = document.querySelectorAll("#navbar ul li a");
-var mainSections = document.querySelectorAll("main section");
+
+let mainNav = document.querySelector("#navbar");
+let mainNavLinks = document.querySelectorAll("#navbar ul li a");
+let mainSections = document.querySelectorAll("main section");
+
 
 window.addEventListener("scroll", function () {
-  let fromTop = window.scrollY;
 
+  let fromTop = window.scrollY;
+  
   mainNavLinks.forEach(function (link) {
     let section = document.querySelector(link.getAttribute('href'));
 
@@ -14,9 +18,25 @@ window.addEventListener("scroll", function () {
       (section.offsetTop + section.offsetHeight) > fromTop
     ) {
 
-      link.classList.add('current');
+      link.classList.add('current', 'visible');
     } else {
-      link.classList.remove('current');
+      link.classList.remove('current', 'visible');
     }
   });
 });
+
+
+
+mainNav.onmouseover = function (){
+  mainNav.classList.add("active");
+  mainNavLinks.forEach(function (link) {
+      link.classList.add('visible');
+    });
+}
+
+mainNav.onmouseout = function (){
+  mainNav.classList.remove("active");
+  mainNavLinks.forEach(function (link) {
+      link.classList.remove('visible');
+    });
+}
